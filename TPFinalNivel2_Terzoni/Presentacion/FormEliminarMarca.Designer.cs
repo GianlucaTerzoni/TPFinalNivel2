b0cff3fe4b4e7e0f501849ddfe2eafbdafa2a484
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2024-03-04 22:37:41 -0300

    formEliminarMarca estilos

--- TPFinalNivel2_Terzoni/Presentacion/FormEliminarMarca.Designer.cs
+++ TPFinalNivel2_Terzoni/Presentacion/FormEliminarMarca.Designer.cs
@@ -38,61 +38,76 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(23, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 16);
+            this.label1.Size = new System.Drawing.Size(275, 21);
             this.label1.TabIndex = 1;
             this.label1.Text = "Seleccione el nombre de la marca";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(23, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 16);
+            this.label2.Size = new System.Drawing.Size(161, 21);
             this.label2.TabIndex = 2;
             this.label2.Text = "que desea eliminar:";
             // 
             // btnEliminarMarca
             // 
+            this.btnEliminarMarca.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnEliminarMarca.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnEliminarMarca.Location = new System.Drawing.Point(26, 270);
+            this.btnEliminarMarca.FlatAppearance.BorderSize = 0;
+            this.btnEliminarMarca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminarMarca.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarMarca.ForeColor = System.Drawing.Color.White;
+            this.btnEliminarMarca.Location = new System.Drawing.Point(42, 270);
             this.btnEliminarMarca.Name = "btnEliminarMarca";
-            this.btnEliminarMarca.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarMarca.Size = new System.Drawing.Size(83, 29);
             this.btnEliminarMarca.TabIndex = 3;
             this.btnEliminarMarca.Text = "Aceptar";
-            this.btnEliminarMarca.UseVisualStyleBackColor = true;
+            this.btnEliminarMarca.UseVisualStyleBackColor = false;
             this.btnEliminarMarca.Click += new System.EventHandler(this.btnEliminarMarca_Click);
             // 
             // btnCancelarEliminarMarca
             // 
+            this.btnCancelarEliminarMarca.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnCancelarEliminarMarca.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnCancelarEliminarMarca.Location = new System.Drawing.Point(125, 270);
+            this.btnCancelarEliminarMarca.FlatAppearance.BorderSize = 0;
+            this.btnCancelarEliminarMarca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelarEliminarMarca.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarEliminarMarca.ForeColor = System.Drawing.Color.White;
+            this.btnCancelarEliminarMarca.Location = new System.Drawing.Point(140, 270);
             this.btnCancelarEliminarMarca.Name = "btnCancelarEliminarMarca";
-            this.btnCancelarEliminarMarca.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelarEliminarMarca.Size = new System.Drawing.Size(83, 29);
             this.btnCancelarEliminarMarca.TabIndex = 4;
             this.btnCancelarEliminarMarca.Text = "Cancelar";
-            this.btnCancelarEliminarMarca.UseVisualStyleBackColor = true;
+            this.btnCancelarEliminarMarca.UseVisualStyleBackColor = false;
             this.btnCancelarEliminarMarca.Click += new System.EventHandler(this.btnCancelarEliminarMarca_Click);
             // 
             // lbxMarcas
             // 
+            this.lbxMarcas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
             this.lbxMarcas.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lbxMarcas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxMarcas.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxMarcas.ForeColor = System.Drawing.Color.White;
             this.lbxMarcas.FormattingEnabled = true;
-            this.lbxMarcas.ItemHeight = 16;
-            this.lbxMarcas.Location = new System.Drawing.Point(26, 72);
+            this.lbxMarcas.ItemHeight = 21;
+            this.lbxMarcas.Location = new System.Drawing.Point(27, 77);
             this.lbxMarcas.Name = "lbxMarcas";
-            this.lbxMarcas.Size = new System.Drawing.Size(189, 180);
+            this.lbxMarcas.Size = new System.Drawing.Size(223, 172);
             this.lbxMarcas.TabIndex = 5;
             // 
             // FormEliminarMarca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(265, 343);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.ClientSize = new System.Drawing.Size(335, 343);
             this.Controls.Add(this.lbxMarcas);
             this.Controls.Add(this.btnCancelarEliminarMarca);
             this.Controls.Add(this.btnEliminarMarca);
