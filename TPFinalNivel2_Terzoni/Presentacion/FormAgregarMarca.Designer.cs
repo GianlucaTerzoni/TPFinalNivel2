91fa0a43daedc5ddc952a3633e3c19af333a8d28
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2024-03-04 22:29:09 -0300

    formAgregarMarca estilos

--- TPFinalNivel2_Terzoni/Presentacion/FormAgregarMarca.Designer.cs
+++ TPFinalNivel2_Terzoni/Presentacion/FormAgregarMarca.Designer.cs
@@ -38,47 +38,60 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(49, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(192, 16);
+            this.label1.Size = new System.Drawing.Size(252, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ingrese el nombre de la marca ";
             // 
             // txtAgregarMarca
             // 
-            this.txtAgregarMarca.Location = new System.Drawing.Point(52, 67);
+            this.txtAgregarMarca.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAgregarMarca.Location = new System.Drawing.Point(53, 79);
             this.txtAgregarMarca.Name = "txtAgregarMarca";
-            this.txtAgregarMarca.Size = new System.Drawing.Size(178, 20);
+            this.txtAgregarMarca.Size = new System.Drawing.Size(210, 27);
             this.txtAgregarMarca.TabIndex = 1;
             // 
             // btnAceptarMarca
             // 
-            this.btnAceptarMarca.Location = new System.Drawing.Point(52, 108);
+            this.btnAceptarMarca.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAceptarMarca.FlatAppearance.BorderSize = 0;
+            this.btnAceptarMarca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptarMarca.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptarMarca.ForeColor = System.Drawing.Color.White;
+            this.btnAceptarMarca.Location = new System.Drawing.Point(61, 131);
             this.btnAceptarMarca.Name = "btnAceptarMarca";
-            this.btnAceptarMarca.Size = new System.Drawing.Size(83, 23);
+            this.btnAceptarMarca.Size = new System.Drawing.Size(93, 23);
             this.btnAceptarMarca.TabIndex = 2;
             this.btnAceptarMarca.Text = "Aceptar";
-            this.btnAceptarMarca.UseVisualStyleBackColor = true;
+            this.btnAceptarMarca.UseVisualStyleBackColor = false;
             this.btnAceptarMarca.Click += new System.EventHandler(this.btnAceptarMarca_Click);
             // 
             // btnCancelarMarca
             // 
-            this.btnCancelarMarca.Location = new System.Drawing.Point(155, 108);
+            this.btnCancelarMarca.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnCancelarMarca.FlatAppearance.BorderSize = 0;
+            this.btnCancelarMarca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelarMarca.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarMarca.ForeColor = System.Drawing.Color.White;
+            this.btnCancelarMarca.Location = new System.Drawing.Point(160, 131);
             this.btnCancelarMarca.Name = "btnCancelarMarca";
-            this.btnCancelarMarca.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelarMarca.Size = new System.Drawing.Size(93, 23);
             this.btnCancelarMarca.TabIndex = 3;
             this.btnCancelarMarca.Text = "Cancelar";
-            this.btnCancelarMarca.UseVisualStyleBackColor = true;
+            this.btnCancelarMarca.UseVisualStyleBackColor = false;
             this.btnCancelarMarca.Click += new System.EventHandler(this.btnCancelarMarca_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(49, 36);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(49, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.Size = new System.Drawing.Size(165, 21);
             this.label2.TabIndex = 4;
             this.label2.Text = "que desea agregar:";
             // 
@@ -86,7 +99,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 164);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.ClientSize = new System.Drawing.Size(320, 187);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnCancelarMarca);
             this.Controls.Add(this.btnAceptarMarca);
